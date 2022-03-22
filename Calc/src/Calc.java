a9e80db26cf06ac31874cd18fb31bda7c7ea4484
Daniel <danielbeatoramirez@gmail.com>
2022-03-22 13:53:58 -0300

    calc_first_class

---
+++ Calc/src/Calc.java
@@ -0,0 +1,8 @@
+public class Calc {
+    public static void showResult(int result) {
+        System.out.println("The result is " + result);
+    }
+    public static int add(int num1, int num2) {
+        return  num1 + num2;
+    }
+}
---
+++ Calc/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] result) {
+        // write your code here
+        System.out.println("Hello world");
+        Calc.showResult(Calc.add(5,4));
+    }
+}
